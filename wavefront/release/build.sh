17228a3c9e3d373296054c90e1d08db7c896c475
Matthew Conger-Eldeen <matthewco@vmware.com>
2021-04-09 14:49:11 -0600

    automate building releases for the wavefront chart (#52)
    Signed-off-by: Mark Michael <mamichael@vmware.com>

---
+++ wavefront/release/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash -e
+
+cd "$(dirname "$0")"
+source ./VERSION
+
+cd .. # ./wavefront
+helm dependency update
+
+cd .. # ./
+./release.sh wavefront
+
+GIT_BRANCH="gh-pages-${CURRENT_CHART_VERSION}"
+
+git fetch
+git checkout -b $GIT_BRANCH origin/gh-pages
+mv ./_build/* .
+git commit -am "build release for ${CURRENT_CHART_VERSION}"
+git push --set-upstream origin $GIT_BRANCH
+
+gh pr create --base gh-pages --fill --head $GIT_BRANCH --web
