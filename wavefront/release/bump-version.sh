4def9393c23b43f9dc6d30c063a5655f208eab90
Matthew Conger-Eldeen <mceldeen@gmail.com>
2021-04-09 11:24:19 -0600

    automatically create when bumping wavefront chart verison (#49)

--- wavefront/release/bump-version.sh
+++ wavefront/release/bump-version.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
-RELEASED_VERSION="1.3.1"
-CURRENT_VERSION="1.3.2"
+cd "$(dirname "$0")"
 
-RELEASED_CHART_VERSION="1.3.8"
-CURRENT_CHART_VERSION="1.3.9"
+source ./VERSION
+GIT_BRANCH="wavefront-bump-${CURRENT_CHART_VERSION}"
+
+git checkout -b $GIT_BRANCH
 
 ## Bump app version
 sed -i "" "s/appVersion: ${RELEASED_VERSION}/appVersion: ${CURRENT_VERSION}/g" ../Chart.yaml
@@ -12,3 +13,9 @@ sed -i "" "s/${RELEASED_VERSION}/${CURRENT_VERSION}/g" ../values.yaml
 
 ## Bump chart version
 sed -i "" "s/version: ${RELEASED_CHART_VERSION}/version: ${CURRENT_CHART_VERSION}/g" ../Chart.yaml
+
+git commit -am "bump helm chart version to $CURRENT_CHART_VERSION"
+
+git push --set-upstream origin $GIT_BRANCH
+
+gh pr create --base master --fill --head $GIT_BRANCH --web
