d07f586fa86e0adefb59a8130a47b8fb459bee15
Priya Selvaganesan <pselvaganesa@vmware.com>
2021-11-05 14:22:35 -0700

    K8SSAAS-492 Create PR for github page release.
    Signed-off-by: Helen Shao <shaoh@vmware.com>

--- wavefront/release/release-helm-chart.sh
+++ wavefront/release/release-helm-chart.sh
@@ -19,7 +19,16 @@ GIT_BRANCH="gh-pages-${CURRENT_CHART_VERSION}"
 git fetch
 git checkout -b $GIT_BRANCH origin/gh-pages
 mv ./_build/* .
-git add . && git commit -am "build release for ${CURRENT_CHART_VERSION}"
+git add . && git commit -am "Build release for ${CURRENT_CHART_VERSION}"
 git push --set-upstream origin $GIT_BRANCH
 
 gh pr create --base gh-pages --fill --head $GIT_BRANCH --web
+
+PR_URL=$(curl \
+  -X POST \
+  -H "Authorization: token ${TOKEN}" \
+  -d "{\"head\":\"${GIT_BRANCH}\",\"base\":\"gh-pages\",\"title\":\"Build release for ${CURRENT_CHART_VERSION}\"}" \
+  https://api.github.com/repos/wavefrontHQ/helm/pulls |
+  jq -r '.url')
+
+echo "PR URL: ${PR_URL}"
