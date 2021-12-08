8f26a4a394fc632e7b412d6724f3940f32169ccf
Priya Selvaganesan <pselvaganesa@vmware.com>
2021-12-08 10:47:00 -0700

    K8SSAAS-650 Upgrade api version in chart and fix e2e test to use correct versions (#132)
    Signed-off-by: Priya Selvaganesan <pselvaganesa@vmware.com>
    Signed-off-by:: Matthew Conger-Eldeen <matthewco@vmware.com>

--- wavefront/release/test-e2e.sh
+++ wavefront/release/test-e2e.sh
@@ -9,7 +9,7 @@ function curl_query_to_wf_dashboard() {
   # NOTE: any output inside this function is concatenated and used as the return value;
   # otherwise we would love to put a log such as this in here to give us more information:
   # echo "=============== Querying '$WF_CLUSTER' for query '${query}'"
-  curl -X GET --header "Accept: application/json" \
+  curl -s -X GET --header "Accept: application/json" \
     --header "Authorization: Bearer $WAVEFRONT_TOKEN" \
     "https://$WF_CLUSTER.wavefront.com/api/v2/chart/api?q=${query}&queryType=WQL&s=$AFTER_UNIX_TS&g=s&view=METRIC&sorted=false&cached=true&useRawQK=false" |
     jq '.timeseries[0].data[0][1]'
@@ -20,21 +20,23 @@ function wait_for_query_match_exact() {
   local expected=$2
   local actual
   local loop_count=0
-  while [[ $actual != "${expected}" ]] && [[ $loop_count -lt $MAX_QUERY_TIMES ]]; do
+
+  printf "Querying for exact match..."
+  while true; do
     loop_count=$((loop_count + 1))
-    echo "===============BEGIN checking wavefront dashboard metrics for '$query_match_exact' - attempt $loop_count/$MAX_QUERY_TIMES"
     actual=$(curl_query_to_wf_dashboard "${query_match_exact}")
-    echo "Actual is: '$actual'"
-    echo "Expected is '${expected}'"
-    echo "===============END checking wavefront dashboard metrics for $query_match_exact"
-
-    if [[ $actual != "${expected}" ]] && [[ $loop_count -lt $MAX_QUERY_TIMES ]]; then
-      sleep $CURL_WAIT
+    printf "."
+    if [[ $actual == "${expected}" ]] || [[ $loop_count -eq $MAX_QUERY_TIMES ]]; then
+      break
     fi
-
+    sleep $CURL_WAIT
   done
+  echo "done."
 
   if [[ $actual != "${expected}" ]]; then
+    echo "Checking wavefront dashboard metrics for '$query_match_exact' failed after attempting $MAX_QUERY_TIMES times."
+    echo "Actual is '$actual'"
+    echo "Expected is '${expected}'"
     return 1
   fi
 }
@@ -43,22 +45,22 @@ function wait_for_query_non_zero() {
   local query_non_zero=$1
   local actual=0
   local loop_count=0
-  while [[ $actual == null || $actual == 0 ]] && [[ $loop_count -lt $MAX_QUERY_TIMES ]]; do
+  printf "Querying for non-zero..."
+  while true; do
     loop_count=$((loop_count + 1))
-
-    echo "===============BEGIN checking wavefront dashboard metrics for $query_non_zero - attempt $loop_count/$MAX_QUERY_TIMES"
     actual=$(curl_query_to_wf_dashboard "${query_non_zero}")
-    echo "Actual is: '$actual'"
-    echo "Expected non zero"
-    echo "===============END checking wavefront dashboard metrics for $query_non_zero"
-
-    if [[ $actual == null || $actual == 0 ]] && [[ $loop_count -lt $MAX_QUERY_TIMES ]]; then
-      sleep $CURL_WAIT
+    printf "."
+    if [[ $actual != null && $actual != 0 ]] || [[ $loop_count -eq $MAX_QUERY_TIMES ]]; then
+      break
     fi
-
+    sleep $CURL_WAIT
   done
+  echo "done."
 
   if [[ $actual == null || $actual == 0 ]]; then
+    echo "Checking wavefront dashboard metrics for $query_non_zero failed after attempting $MAX_QUERY_TIMES times."
+    echo "Actual is '$actual'"
+    echo "Expected non zero"
     return 1
   fi
 }
@@ -69,14 +71,16 @@ function print_usage_and_exit() {
   echo -e "\t-c wavefront instance name (default: 'nimba')"
   echo -e "\t-t wavefront token (required)"
   echo -e "\t-n config cluster name for metric grouping (default: \$(whoami)-<default version from file>-release-test)"
-  echo -e "\t-v collector docker image version (default: load from 'release/VERSION')"
+  echo -e "\t-v collector version (default: load from 'release/VERSION')"
   exit 1
 }
 
 function exit_on_fail() {
+  # shellcheck disable=SC2068
   $@ # run all arguments as a command
   local exit_code=$?
   if [[ $exit_code != 0 ]]; then
+    # shellcheck disable=SC2145
     echo "Command '$@' exited with exit code '$exit_code'"
     exit $exit_code
   fi
@@ -93,9 +97,9 @@ function main() {
   local WAVEFRONT_TOKEN=
 
   local WF_CLUSTER=nimba
-  local VERSION=${CURRENT_VERSION}
+  local EXPECTED_VERSION=${APP_VERSION}
 
-  while getopts ":c:t:n:" opt; do
+  while getopts ":c:t:n:v:" opt; do
     case $opt in
     c)
       WF_CLUSTER="$OPTARG"
@@ -105,6 +109,9 @@ function main() {
       ;;
     n)
       CONFIG_CLUSTER_NAME="$OPTARG"
+      ;;
+    v)
+      EXPECTED_VERSION="$OPTARG"
       ;;
     \?)
       print_usage_and_exit "Invalid option: -$OPTARG"
@@ -120,8 +127,8 @@ function main() {
     print_msg_and_exit "config cluster name required"
   fi
 
-  local VERSION_IN_DECIMAL="${VERSION%.*}"
-  local VERSION_IN_DECIMAL+="$(echo "${VERSION}" | cut -d '.' -f3)"
+  local VERSION_IN_DECIMAL="${EXPECTED_VERSION%.*}"
+  local VERSION_IN_DECIMAL+="$(echo "${EXPECTED_VERSION}" | cut -d '.' -f3)"
 
   wait_for_cluster_ready
 
