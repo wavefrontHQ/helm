2a050c299edd95f700e7e7f8e79b5b11b1fdd561
Mark Michael <mamichael@vmware.com>
2021-12-21 11:09:28 -0700

    add generate report script
    Signed-off-by: Helen Shao <shaoh@vmware.com>

---
+++ wavefront/release/run-chart-verifier-generate-report.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+source "$REPO_ROOT"/wavefront/release/k8s-utils.sh
+
+cd "$REPO_ROOT"
+
+yellow "NOTE: Ensure that your current kube config is targeting a clean environment with an IP accessible from the docker container"
+yellow "NOTE: Typical usage would be to set your context to a GKE or EKS cluster"
+
+pushd "$REPO_ROOT/wavefront"
+helm dependency update
+popd
+
+${REPO_ROOT}/release.sh wavefront
+
+VERIFY_YAML=$(docker run --rm \
+  -v "$REPO_ROOT":/charts \
+  -v ~/.kube/config:/etc/kubernetes/config \
+  -e KUBECONFIG=/etc/kubernetes/config \
+  quay.io/redhat-certification/chart-verifier verify \
+    --set chart-testing.namespace=wavefront \
+    --openshift-version 4.9 \
+    --output yaml \
+    --chart-set clusterName=test,wavefront.url=test,wavefront.token=test \
+  "/charts/_build/wavefront-1.7.10.tgz" 2>&1)
+
+echo "${VERIFY_YAML}" >"${REPO_ROOT}/_build/report.yaml"
