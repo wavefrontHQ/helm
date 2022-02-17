a612da6e4c47bef121f2c5d82c8c4ba16813edb9
jcornish <jcornish@vmware.com>
2022-02-17 15:58:15 -0700

    our environment is contaminated :D

--- wavefront/release/setup-for-testing.sh
+++ wavefront/release/setup-for-testing.sh
@@ -4,6 +4,7 @@
 # npm
 #
 curl https://nodejs.org/dist/v16.14.0/node-v16.14.0-linux-arm64.tar.gz | tar xz
+sudo rm /usr/local/bin/npm
 sudo ln -s ${PWD}/node-v16.14.0-linux-arm64/lib/node_modules/npm/bin/npm-cli.js /usr/local/bin/npm
 
 ls -lAhtr /usr/local/bin
