ca8d9fea65fe49b09dd77d8d5656f54ca31fc89d
jcornish <jcornish@vmware.com>
2022-02-17 16:16:58 -0700

    lol not arm

--- wavefront/release/setup-for-testing.sh
+++ wavefront/release/setup-for-testing.sh
@@ -3,9 +3,9 @@
 #
 # npm
 #
-curl https://nodejs.org/dist/v16.14.0/node-v16.14.0-linux-arm64.tar.gz | tar xz
+curl https://nodejs.org/dist/v16.14.0/node-v16.14.0-linux-x64.tar.gz | tar xz
 
-export PATH=$PATH:${PWD}/node-v16.14.0-linux-arm64/bin
+export PATH=$PATH:${PWD}/node-v16.14.0-linux-x64/bin
 
 ls -lAhtr /usr/local/bin
 echo $PATH
