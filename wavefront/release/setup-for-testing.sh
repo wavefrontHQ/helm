55f3aa47b361ee67b848b40396f05b3521208d73
jcornish <jcornish@vmware.com>
2022-02-17 15:18:06 -0700

    dont furget the sooper dewer

--- wavefront/release/setup-for-testing.sh
+++ wavefront/release/setup-for-testing.sh
@@ -4,6 +4,6 @@
 # npm
 #
 curl https://nodejs.org/dist/v16.14.0/node-v16.14.0-linux-arm64.tar.gz | tar xz
-mv node-v16.14.0-linux-arm64/bin/npm /usr/local/bin/npm
+sudo mv node-v16.14.0-linux-arm64/bin/npm /usr/local/bin/npm
 
 npm version
