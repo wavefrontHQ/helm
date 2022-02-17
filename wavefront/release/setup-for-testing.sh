d31110ee97c4570b2cdc487f89af825c1d3c0fd1
jcornish <jcornish@vmware.com>
2022-02-17 15:40:39 -0700

    ag

--- wavefront/release/setup-for-testing.sh
+++ wavefront/release/setup-for-testing.sh
@@ -4,7 +4,6 @@
 # npm
 #
 curl https://nodejs.org/dist/v16.14.0/node-v16.14.0-linux-arm64.tar.gz | tar xz
-sudo mv node-v16.14.0-linux-arm64/bin/npm /usr/local/bin/npm
 sudo ln -s /usr/local/bin/npm ${PWD}/node-v16.14.0-linux-arm64/bin/npm
 
 ls -lAhtr /usr/local/bin
