905ea733a220d351bfb9e58d26cb1479a259a12d
Anil Kodali <akodali@vmware.com>
2022-04-06 08:51:41 -0700

    WIP: rename files and refactor as per jenkins file stage requirement.

---
+++ wavefront/openshift/bump-version.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+
+#NEW_CHART_VERSION=1.10.3-test
+
+# Update values, chart and readme
+cd "$(dirname "$0")"
+source ./VERSION
+
+OLD_APP_VERSION=$APP_VERSION
+OLD_CHART_VERSION=$CHART_VERSION
+
+# Create a branch with the new chart version name
+GIT_BRANCH="wavefront-bump-openshift-${NEW_CHART_VERSION}"
+git checkout -b $GIT_BRANCH
+
+# Update VERSION file with new versions
+sed -i "s/APP_VERSION=\"${OLD_APP_VERSION}\"/APP_VERSION=\"${NEW_APP_VERSION}\"/g" VERSION
+sed -i "s/CHART_VERSION=\"${OLD_CHART_VERSION}\"/CHART_VERSION=\"${NEW_CHART_VERSION}\"/g" VERSION
+
+# Bump app version
+sed -i "s/appVersion: ${OLD_APP_VERSION}/appVersion: ${NEW_APP_VERSION}/g" ../Chart.yaml
+sed -i "s/${OLD_APP_VERSION}/${NEW_APP_VERSION}/g" ../values.yaml
+
+## Bump chart version
+sed -i "s/version: ${OLD_CHART_VERSION}/version: ${NEW_CHART_VERSION}/g" ../Chart.yaml
+sed -i "s/${OLD_CHART_VERSION}/${NEW_CHART_VERSION}/g" ../release/run-chart-verifier-generate-report.sh.yaml
+
+git commit -am "Bump OpenShift helm chart version to $NEW_CHART_VERSION"
+
+git push --set-upstream origin $GIT_BRANCH
+
+PR_URL=$(curl \
+  -X POST \
+  -H "Authorization: token ${TOKEN}" \
+  -d "{\"head\":\"${GIT_BRANCH}\",\"base\":\"master\",\"title\":\"Bump helm chart version to ${NEW_CHART_VERSION}\",\"description\":\"1. Ensure that any schema.json changes have been made for new properties \n 2.Ensure that any  ./wavefront/README.md changes have made it to ./wavefront/openshift/README.md\"}" \
+  https://api.github.com/repos/wavefrontHQ/helm/pulls |
+  jq -r '.html_url')
+
+echo "Please verify and merge PR : ${PR_URL}"
+
--- wavefront/openshift/run-chart-verifier-test-remote.sh
+++ wavefront/openshift/run-chart-verifier-test-remote.sh
@@ -3,45 +3,6 @@
 OPENSHIFT_DEV_PWD=1Wavefront!
 NEW_CHART_VERSION=1.10.3-test
 
-# Update values, chart and readme
-cd "$(dirname "$0")"
-source ./VERSION
-
-OLD_APP_VERSION=$APP_VERSION
-OLD_CHART_VERSION=$CHART_VERSION
-
-# Create a branch with the new chart version name
-GIT_BRANCH="wavefront-bump-openshift-${NEW_CHART_VERSION}"
-git checkout -b $GIT_BRANCH
-
-# Update VERSION file with new versions
-sed -i "s/APP_VERSION=\"${OLD_APP_VERSION}\"/APP_VERSION=\"${NEW_APP_VERSION}\"/g" VERSION
-sed -i "s/CHART_VERSION=\"${OLD_CHART_VERSION}\"/CHART_VERSION=\"${NEW_CHART_VERSION}\"/g" VERSION
-
-# Bump app version
-sed -i "s/appVersion: ${OLD_APP_VERSION}/appVersion: ${NEW_APP_VERSION}/g" ../Chart.yaml
-sed -i "s/${OLD_APP_VERSION}/${NEW_APP_VERSION}/g" ../values.yaml
-
-## Bump chart version
-sed -i "s/version: ${OLD_CHART_VERSION}/version: ${NEW_CHART_VERSION}/g" ../Chart.yaml
-sed -i "s/${OLD_CHART_VERSION}/${NEW_CHART_VERSION}/g" ../release/run-chart-verifier-generate-report.sh.yaml
-
-# TODO (provide a warning): Ensure that any schema.json changes have been made for new properties
-# TODO (provide a warning): Ensure that any  ./wavefront/README.md changes have made it to ./wavefront/openshift/README.md
-
-git commit -am "Bump OpenShift helm chart version to $NEW_CHART_VERSION"
-
-git push --set-upstream origin $GIT_BRANCH
-
-PR_URL=$(curl \
-  -X POST \
-  -H "Authorization: token ${TOKEN}" \
-  -d "{\"head\":\"${GIT_BRANCH}\",\"base\":\"master\",\"title\":\"Bump helm chart version to ${NEW_CHART_VERSION}\"}" \
-  https://api.github.com/repos/wavefrontHQ/helm/pulls |
-  jq -r '.html_url')
-
-echo "PR URL: ${PR_URL}"
-
 # Runs the chart-verifier test
 # TODO: Revisit if we should send password (install other tools like sshpass?)
 # TODO-cont: or setup ssh keys in the VM instead
@@ -51,7 +12,7 @@ if [ $retcode -ne 0 ]; then
   echo "Chart verification failed with exit code $retcode."
   exit $retcode
 fi
-
+#
 # TODO for K8SSAAS-816 from here onwards.
 # 1st time only - Create a fork for and subsequently rebase with upstream and clone to jenkins
 #sh '''
@@ -61,7 +22,7 @@ fi
 #      "https://api.github.com/repos/openshift-helm-charts/charts/forks")
 #'''
 # git clone https://github.com/wf-jenkins/charts.git
-
+#
 #cd charts
 #git remote add upstream https://github.com/openshift-helm-charts/charts
 #git fetch upstream
