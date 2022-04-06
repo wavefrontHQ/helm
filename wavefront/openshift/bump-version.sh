3fb249b30693ae23099b8b2f70260e1e2d66d9e5
Priya Selvaganesan <pselvaganesa@vmware.com>
2022-04-06 15:40:37 -0700

    pwd to lcoate where we are at now

--- wavefront/openshift/bump-version.sh
+++ wavefront/openshift/bump-version.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-#NEW_CHART_VERSION=1.10.3-test
-
-# Update values, chart and readme
 cd "$(dirname "$0")"
 source ./VERSION
 
@@ -20,7 +17,8 @@ sed -i "s/CHART_VERSION=\"${OLD_CHART_VERSION}\"/CHART_VERSION=\"${NEW_CHART_VER
 # Bump app version
 sed -i "s/appVersion: ${OLD_APP_VERSION}/appVersion: ${NEW_APP_VERSION}/g" ../Chart.yaml
 sed -i "s/${OLD_APP_VERSION}/${NEW_APP_VERSION}/g" ../values.yaml
-
+echo pwd
+pwd
 ## Bump chart version
 sed -i "s/version: ${OLD_CHART_VERSION}/version: ${NEW_CHART_VERSION}/g" ../Chart.yaml
 sed -i "s/${OLD_CHART_VERSION}/${NEW_CHART_VERSION}/g" ../release/run-chart-verifier-generate-report.sh.yaml
