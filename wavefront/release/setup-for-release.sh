05f180ae41ddccf0bbd1e7018b460f699542111e
Priya Selvaganesan <pselvaganesa@vmware.com>
2022-04-06 15:25:11 -0700

    Remove sshpass

--- wavefront/release/setup-for-release.sh
+++ wavefront/release/setup-for-release.sh
@@ -14,8 +14,3 @@ fi
 #
 curl https://get.helm.sh/helm-v3.6.3-linux-amd64.tar.gz | tar xz --to-stdout linux-amd64/helm | sudo tee /usr/local/bin/helm >/dev/null
 sudo chmod +x /usr/local/bin/helm
-
-#
-# sshpass
-#
-curl -L https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb > sshpass.rb && brew install sshpass.rb && rm sshpass.rb
