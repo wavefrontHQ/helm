b99415cde4198d95187a2eec815f13667089b5ac
Anil Kodali <akodali@vmware.com>
2022-04-08 10:58:31 -0700

    remove strict host checking
    Signed-off-by: Helen Shao <shaoh@vmware.com>

--- wavefront/openshift/openshift-build-report.sh
+++ wavefront/openshift/openshift-build-report.sh
@@ -5,6 +5,6 @@
 # TODO-cont: or setup ssh keys in the VM instead
 # TODO-cont: or use a Jenkins plugin
 
-sudo yum install sshpass
-sshpass -p "${OPENSHIFT_CREDS_PSW}" ssh root@${OPENSHIFT_VM} 'bash -s' < wavefront/openshift/test-and-generate-report.sh
+#sudo yum install sshpass
+sshpass -p "${OPENSHIFT_CREDS_PSW}" ssh -o StrictHostKeyChecking=no -tt root@${OPENSHIFT_VM} 'bash -s' < wavefront/openshift/test-and-generate-report.sh
 
