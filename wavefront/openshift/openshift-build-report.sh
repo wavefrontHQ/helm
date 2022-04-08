e6350c907973ed1e572998a89650788eec1826a6
Anil Kodali <akodali@vmware.com>
2022-04-08 09:41:10 -0700

    modify script path
    Signed-off-by: Helen Shao <shaoh@vmware.com>

--- wavefront/openshift/openshift-build-report.sh
+++ wavefront/openshift/openshift-build-report.sh
@@ -4,7 +4,7 @@
 # TODO: Revisit if we should send password (install other tools like sshpass?)
 # TODO-cont: or setup ssh keys in the VM instead
 # TODO-cont: or use a Jenkins plugin
-retcode=$(sshpass -p "${OPENSHIFT_CREDS_PSW}" ssh root@${OPENSHIFT_VM} 'bash -s' < ./test-and-generate-report.sh)
+retcode=$(sshpass -p "${OPENSHIFT_CREDS_PSW}" ssh root@${OPENSHIFT_VM} 'bash -s' < test-and-generate-report.sh)
 if [ $retcode -ne 0 ]; then
   echo "Chart verification failed with exit code $retcode."
   exit $retcode
