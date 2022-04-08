d85e04ad97dafa019a149f80caf532eafdcc7339
Anil Kodali <akodali@vmware.com>
2022-04-08 10:12:51 -0700

    install sshpass
    Signed-off-by: Helen Shao <shaoh@vmware.com>

--- wavefront/openshift/openshift-build-report.sh
+++ wavefront/openshift/openshift-build-report.sh
@@ -4,8 +4,8 @@
 # TODO: Revisit if we should send password (install other tools like sshpass?)
 # TODO-cont: or setup ssh keys in the VM instead
 # TODO-cont: or use a Jenkins plugin
-curl -L https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb > sshpass.rb && brew install sshpass.rb && rm sshpass.rb
 
+sudo apt-get install sshpass
 retcode=$(sshpass -p "${OPENSHIFT_CREDS_PSW}" ssh root@${OPENSHIFT_VM} 'bash -s' < wavefront/openshift/test-and-generate-report.sh)
 if [ $retcode -ne 0 ]; then
   echo "Chart verification failed with exit code $retcode."
