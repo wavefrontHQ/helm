98f5df7655d697612d04191dbfafe9d1a9117ac0
Anil Kodali <akodali@vmware.com>
2022-04-11 16:14:10 -0700

    xx

--- wavefront/openshift/openshift-release.sh
+++ wavefront/openshift/openshift-release.sh
@@ -27,29 +27,30 @@ git push origin main --force
 # Update fork with the new chart version.
 GIT_BRANCH=main
 echo ${CHART_VERSION}
-mkdir charts/partners/wavefronthq/wavefront/1.10.2-test1
+mkdir charts/partners/wavefronthq/wavefront/${CHART_VERSION}
 echo "downloading tgz"
-#sshpass -p "${OPENSHIFT_CREDS_PSW}" scp root@${OPENSHIFT_VM}:/root/workspace/helm/_build/wavefront-${CHART_VERSION}.tgz charts/partners/wavefronthq/wavefront/1.10.2-test1
+sshpass -p "${OPENSHIFT_CREDS_PSW}" scp root@${OPENSHIFT_VM}:/root/workspace/helm/_build/wavefront-${CHART_VERSION}.tgz charts/partners/wavefronthq/wavefront/${CHART_VERSION}
 echo "downloading report"
-sshpass -p "${OPENSHIFT_CREDS_PSW}" scp root@${OPENSHIFT_VM}:/root/workspace/helm/_build/report.yaml charts/partners/wavefronthq/wavefront/1.10.2-test1
+sshpass -p "${OPENSHIFT_CREDS_PSW}" scp root@${OPENSHIFT_VM}:/root/workspace/helm/_build/report.yaml charts/partners/wavefronthq/wavefront/${CHART_VERSION}
 
 # Commit and push the change to your forked version
 # Create a new PR against https://github.com/openshift-helm-charts/charts and this should trigger the openshift pipeline
 
-git add . && git commit -am "Build openshift wavefront chart release : 1.10.2-test1"
+git add . && git commit -am "Build openshift wavefront chart release : ${CHART_VERSION}"
 git push origin $GIT_BRANCH
 
-echo "Creating PR"
-PR_URL=$(curl \
-  -X POST \
-  -H "Authorization: token ${GITHUB_CREDS_PSW}" \
-  -d "{\"head\":\"wf-jenkins:${GIT_BRANCH}\",\"base\":\"main\",\"title\":\"DO NOT MERGE - Test Build openshift wavefront chart release 1.10.2-test1\"}" \
-  https://api.github.com/repos/openshift-helm-charts/charts/pulls |
-  jq -r '.html_url')
-
-echo "Next Human Steps :: Please monitor openshift pipeline for PR - ${PR_URL}"
-echo "Next Human Steps :: If above PR fails due to permission issues, please create
- a PR manually to https://github.com/openshift-helm-charts/charts from https://github.com/wf-jenkins/charts:main"
+echo "NOTE :: Currently wf-jenkins is not allowed to submit the chart on behalf of wavefronthq"
+# TODO:: Uncomment the below code after wf-jenkins is added as a contributor to wavefronthq helm chart
+#PR_URL=$(curl \
+#  -X POST \
+#  -H "Authorization: token ${GITHUB_CREDS_PSW}" \
+#  -d "{\"head\":\"wf-jenkins:${GIT_BRANCH}\",\"base\":\"main\",\"title\":\"DO NOT MERGE - Test Build openshift wavefront chart release ${CHART_VERSION}\"}" \
+#  https://api.github.com/repos/openshift-helm-charts/charts/pulls |
+#  jq -r '.html_url')
+#
+#
+#echo "Next Human Steps :: Please monitor openshift pipeline for PR - ${PR_URL}"
+echo "Next Human Steps :: Please create a PR manually to https://github.com/openshift-helm-charts/charts from https://github.com/wf-jenkins/charts:main"
 
 
 
