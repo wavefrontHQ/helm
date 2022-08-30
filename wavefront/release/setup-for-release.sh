4534af479a8798f09247117f6ccd1c44f7cf68cb
Priya Selvaganesan <1836417+priyaselvaganesan@users.noreply.github.com>
2022-08-30 14:58:10 -0700

    Install yq (#245)
    Co-authored-by: Helen Shao <shaoh@vmware.com>

--- wavefront/release/setup-for-release.sh
+++ wavefront/release/setup-for-release.sh
@@ -10,6 +10,15 @@ if ! [ -x "$(command -v jq)" ]; then
 fi
 
 #
+# yq
+#
+if ! [ -x "$(command -v yq)" ]; then
+  curl -H "Authorization: token ${GITHUB_CREDS_PSW}" -L "https://github.com/mikefarah/yq/releases/download/v4.27.3/yq_linux_amd64" > ./yq
+  chmod +x ./yq
+  sudo mv ./yq /usr/local/bin
+fi
+
+#
 # helm
 #
 curl https://get.helm.sh/helm-v3.6.3-linux-amd64.tar.gz | tar xz --to-stdout linux-amd64/helm | sudo tee /usr/local/bin/helm >/dev/null
